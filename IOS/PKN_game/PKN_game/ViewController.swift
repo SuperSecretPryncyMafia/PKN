c27071854cde827e7fa3696f3305667a89d652ee
Mona Lisa <shepherd90@windowslive.com>
2021-09-06 21:49:32 +0200

    2nd commsss

--- IOS/PKN_game/PKN_game/ViewController.swift
+++ IOS/PKN_game/PKN_game/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Paper: UIButton!
+    
+    @IBOutlet weak var scissors: UIButton!
+    @IBOutlet weak var rock: UIButton!
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    var imageNum = 1
+    
+    
+   
+
+    let imagesArray = [ #imageLiteral(resourceName: "paper"), #imageLiteral(resourceName: "rock"), #imageLiteral(resourceName: "scissors") ]
+    
+    
+    
+    @IBAction func paperClicked(_ sender: UIButton) {
+        
+        imageView.image = imagesArray.first
+        
+        
+    }
+    
+    
+    
+    @IBAction func rockClicked(_ sender: UIButton) {
+        
+        imageView.image = imagesArray[1]
+        
+    }
+    
+    
+    @IBAction func scissorsClicked(_ sender: UIButton) {
+        imageView.image = imagesArray.last
+    }
+    
 // pisze sobie cos test test, test na Sziperd-branch, test 2
 }
 
